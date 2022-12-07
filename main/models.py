ac66b5ad0edc24dd8acd5336091610e01968d7eb
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2022-12-07 17:25:56 +0100

    added registration numbers

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    reg_number = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.reg_number
--- main/views.py
+++ main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Member
 
 # Create your views here.
 def home_view(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/index.html', {'members':Member.objects.all()})
+
+def detail_view(request, id):
+    return render(request, 'main/detail.html', {'member':Member.objects.get(id=id)})
+
